b03d1f73ad6b5ab63ce5c6da3bc733cf61a1d574
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-05-25 20:12:58 +0200

    aggiornamento table

--- database/migrations/2023_05_25_172435_create_trains_table.php
+++ database/migrations/2023_05_25_172435_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->string('Codice treno');
+            $table->tinyInteger('Numero carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Canecellato');
             $table->timestamps();
         });
     }
